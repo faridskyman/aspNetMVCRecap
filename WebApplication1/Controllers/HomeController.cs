6a3dc74ef1b8a8fdfc0cf916cedda4c4d4e1a2bb
farid <farid.skyman@gmail.com>
2015-12-09 22:33:26 +0800

    init asp.net mvc project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Demo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            // return Content(serial);
+            // return new HttpStatusCodeResult(403);
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+
+           // full list of action results
+        }
+    }
+}
